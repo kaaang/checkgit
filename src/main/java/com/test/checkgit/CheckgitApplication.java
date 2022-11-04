fe17180509427d8a69f0cda255129385b19f1157
kaaang <kaaang@kakao.com>
2022-11-04 20:48:40 +0900

    Initial commit

---
+++ src/main/java/com/test/checkgit/CheckgitApplication.java
@@ -0,0 +1,13 @@
+package com.test.checkgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckgitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/checkgit/CheckgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.checkgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
